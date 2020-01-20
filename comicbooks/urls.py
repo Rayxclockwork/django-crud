b8ebdb315874ca9eaf4597f3c545e320a219dfb3
Raven Delaney <ravend17@gmail.com>
2020-01-20 14:11:16 -0800

    Update urls.py

--- comicbooks/urls.py
+++ comicbooks/urls.py
@@ -4,7 +4,7 @@ from .views import EntryList, EntryDetail, BookCreateView, BookUpdateView, BookD
 urlpatterns = [
 	path('', EntryList.as_view(), name = 'home'),
 	path('detail/<int:pk>/', EntryDetail.as_view(), name = 'detail'),
-	path('book/new/', BookCreateView.as_view(), name='add_bottle'),
-    path('update/<int:pk>', BookUpdateView.as_view(), name='update_bottle'),
-    path('delete/<int:pk>', BookDeleteView.as_view(), name='delete_bottle')
+	path('book/new/', BookCreateView.as_view(), name='add'),
+    path('update/<int:pk>', BookUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', BookDeleteView.as_view(), name='delete')
 ]
